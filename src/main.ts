5f98f01df80a675aed8179931432a10280e679ad
NaingHtetHtun <axel8102005@gmail.com>
2026-02-19 08:35:05 +0630

    deployment on vercel

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getBotToken } from 'nestjs-telegraf';
+import { Telegraf } from 'telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const bot = app.get<Telegraf>(getBotToken());
+
+  app.use(bot.webhookCallback('/api/webhook'));
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
